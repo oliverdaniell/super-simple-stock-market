dd38ea9ae547b70f460939f5f5e3482390faf747
Oliver <oliver.daniell@seebyte.com>
2017-06-17 16:46:34 +0100

    added test for 1.a.ii

--- unit-tests/stocks_test.cpp
+++ unit-tests/stocks_test.cpp
@@ -27,3 +27,27 @@ TEST_CASE("Given any price as input, calculate the dividend yield","[1.a.i]")
 		REQUIRE(stocks::dividend_yield_common(250.0, 13.0) == Approx(0.052));
 	}
 }
+
+TEST_CASE("Given any price as input, calculate the P/E Ratio", "[1.a.ii]")
+{
+	SECTION("TEA")
+	{
+		REQUIRE(std::isinf( stocks::price_earnings_ratio(100.0, 0.0)));
+	}
+	SECTION("POP")
+	{
+		REQUIRE(stocks::price_earnings_ratio(100.0, 8.0) == Approx(12.5));
+	}
+	SECTION("ALE")
+	{
+		REQUIRE(stocks::price_earnings_ratio(60.0, 23.0) == Approx(2.61).epsilon(0.01));
+	}
+	SECTION("GIN")
+	{
+		REQUIRE(stocks::price_earnings_ratio(100.0, 2.0) == Approx(50.0));
+	}
+	SECTION("JOE")
+	{
+		REQUIRE(stocks::price_earnings_ratio(250.0, 13.0) == Approx(19.2).epsilon(0.01));
+	}
+}
