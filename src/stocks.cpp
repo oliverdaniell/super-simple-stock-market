de2aa16d76cd6aeb33116c7a80f550362a7fab55
Oliver <oliver.daniell@seebyte.com>
2017-06-17 16:47:58 +0100

    implemented 1.a.ii

--- src/stocks.cpp
+++ src/stocks.cpp
@@ -5,20 +5,22 @@ namespace stocks
 {
 double dividend_yield_common(double price, double last_dividend)
 {
-	assert(price > 0.0);
-	assert(last_dividend >= 0.0);
-	return last_dividend / price;
+    assert(price > 0.0);
+    assert(last_dividend >= 0.0);
+    return last_dividend / price;
 }
 double dividend_yield_preferred(double price, double fixed_dividend_fraction, double par_value)
 {
-	assert(price > 0.0);
-	assert(fixed_dividend_fraction >= 0.0);
-	assert(par_value >= 0.0);
-	return fixed_dividend_fraction*par_value / price;
+    assert(price > 0.0);
+    assert(fixed_dividend_fraction >= 0.0);
+    assert(par_value >= 0.0);
+    return fixed_dividend_fraction * par_value / price;
 }
 double price_earnings_ratio(double price, double dividend)
 {
-    return 0.0;
+	assert(price > 0.0);
+	assert(dividend >= 0.0);
+    return price / dividend;
 }
 trade::trade(timestamp_type const &timestamp, int quantity_of_shares, buy_sell_indicator buy_sell,
              double traded_price)
